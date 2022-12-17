ddfa7bf306d8db869f20c0de4468fe78098302ec
DeniD-star <dimitrova.denise@gmail.com>
2022-12-17 14:58:29 +0100

    create, edit, details, delete, rent, not-found page ready

--- controllers/housingController.js
+++ controllers/housingController.js
@@ -1,8 +1,136 @@
 const router = require('express').Router();
-const{ parseError} = require('../util/parse')
+const { isUser } = require('../middlewares/guards');
+const { parseError } = require('../util/parse')
 
-router.get('/catalog', async(req, res)=>{
+router.get('/catalog', async (req, res) => {
     const housings = await req.storage.getAllHousings();
-    res.render('catalog', {housings})
+    res.render('catalog', { housings })
+})
+
+router.get('/create', isUser(), (req, res) => {
+    res.render('create')
+})
+router.post('/create', isUser(), async (req, res) => {
+    try {
+        const housingData = {
+            name: req.body.name,
+            type: req.body.type,
+            year: Number(req.body.year),
+            city: req.body.city,
+            imageUrl: req.body.imageUrl,
+            description: req.body.description,
+            availablePieces: Number(req.body.availablePieces),
+            rentedHome: [],
+            owner: req.user._id,
+        }
+
+        await req.storage.createHousing(housingData);
+        res.redirect('/housings/catalog')
+    } catch (err) {
+        console.log(err.message);
+        const ctx = {
+            errors: parseError(err),
+            housingData: {
+                name: req.body.name,
+                type: req.body.type,
+                year: Number(req.body.year),
+                city: req.body.city,
+                homeImage: req.body.homeImage,
+                description: req.body.description,
+                availablePieces: Number(req.body.availablePieces),
+                owner: req.user._id,
+                rentedHome: [],
+                _id: req.params.id,
+
+            }
+
+        }
+
+        res.render('create', ctx)
+    }
+})
+
+
+router.get('/details/:id', async (req, res) => {
+    try {
+        const housing = await req.storage.getHousingById(req.params.id);
+        housing.hasUser = Boolean(req.user);
+        housing.isOwner = req.user && req.user._id == housing.owner;
+        housing.userRentedHome = req.user && housing.rentedHome.find(x => x._id == req.user._id)
+        housing.available = housing.availablePieces > 0;
+        housing.rentedUsers = housing.rentedHome.map(x=> x.name).join(', ')
+        res.render('details', { housing })
+    } catch (err) {
+        console.log(err.message);
+        res.redirect('/housings/404')
+
+    }
+})
+
+
+router.get('/404', (req, res) => {
+    res.render('404')
+})
+router.get('/edit/:id', isUser(), async (req, res) => {
+    const housing = await req.storage.getHousingById(req.params.id);
+    res.render('edit', {housing})
+})
+router.post('/edit/:id', isUser(), async(req, res) => {
+    try {
+
+        const housing = await req.storage.getHousingById(req.params.id);
+        if (housing.owner != req.user._id) {
+            throw new Error('You cannot edit a housing that is not yours!')
+        }
+
+        await req.storage.editHousing(req.params.id, req.body)
+        res.redirect('/housings/details/' + req.params.id)
+    } catch (err) {
+        const ctx = {
+            errors: parseError(err),
+            housing: {
+                _id: req.params.id,
+                name: req.body.name,
+                type: req.body.type,
+                year: Number(req.body.year),
+                city: req.body.city,
+                homeImage: req.body.homeImage,
+                description: req.body.description,
+                availablePieces: Number(req.body.availablePieces),
+                // owner: req.user._id,
+                // rentedHome: [],
+            }
+        }
+        res.render('edit', ctx)
+    }
+})
+
+router.get('/delete/:id', isUser(), async(req, res)=>{
+    try {
+        const housing = await req.storage.getHousingById(req.params.id);
+        if (housing.owner != req.user._id) {
+            throw new Error('You cannot delete a housing that is not yours!')
+        }
+
+        await req.storage.deleteHousing(req.params.id);
+        res.redirect('/housings/catalog')
+    } catch (err) {
+        console.log(err.message);
+        res.redirect('/housings/404')
+    }
+})
+router.get('/rent/:id', isUser(), async(req, res)=>{
+    try {
+        const housing = await req.storage.getHousingById(req.params.id);
+        if (housing.owner == req.user._id) {
+            throw new Error('You cannot rent a housing that you created!')
+        }
+
+        await req.storage.rentHousing(req.params.id, req.user._id);
+        res.redirect('/housings/details/' + req.params.id)
+    } catch (err) {
+        console.log(err.message);
+        res.redirect('/housings/404')
+    }
 })
 module.exports = router;
--- services/housingService.js
+++ services/housingService.js
@@ -15,8 +15,49 @@ async function getAllHousings(){
 
         
 }
+async function createHousing(housingData){
+    const housing= new Housing(housingData);
+    return housing.save();
 
+        
+}
+
+
+async function getHousingById(id){
+    return Housing.findById(id).populate('rentedHome').lean()
+}
+
+async function editHousing(housingId, newData){
+        const housing = await Housing.findById(housingId);
+        housing.name = newData.name;
+        housing.type = newData.type;
+        housing.year = Number(newData.year);
+        housing.city = newData.city;
+        housing.imageUrl = newData.imageUrl;
+        housing.description = newData.description;
+        housing.availablePieces = Number(newData.availablePieces);
+
+        return housing.save();
+}
+
+async function deleteHousing(id){
+    return Housing.findByIdAndDelete(id);
+}
+
+async function rentHousing(housingId, userId){
+        const housing = await Housing.findById(housingId);
+        housing.rentedHome.push(userId);
+        housing.availablePieces--;
+
+        return housing.save()
+
+}
 module.exports = {
     getLatestHousings,
-    getAllHousings
+    getAllHousings,
+    createHousing,
+    getHousingById,
+    editHousing,
+    deleteHousing,
+    rentHousing
 }
